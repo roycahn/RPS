5da1440f15e542e9676987bd4bf00c5d7a2d83a9
roycahn <royc621@gmail.com>
2016-12-14 18:11:43 -0600

    whoWon Not working

--- assets/javascript/rps.js
+++ assets/javascript/rps.js
@@ -3,7 +3,8 @@ var game =
   { 
     
     pushKey: {},  //used on page close or with game.pushKey.update()
-    turn: "your",
+    turn: "Your",
+    pflag:  false,
     player:       
       {
       name:           "",
@@ -17,6 +18,7 @@ var game =
   }; //game object
 
 var weaponsArray = [];
+
 
 // Initialize Firebase
 var config = {
@@ -37,28 +39,6 @@ $(".game-info").css("visibility","hidden");
 $(".weapons").css("visibility","hidden");
    
   
-//Get username from console and add to firebase
-$("#start-btn").on("click", function() {
-  game.player.name = $("#player-name").val().trim();
-  console.log(game.player.name);
-  
-  //function to add a player to firebase
-  if(game.player.playerNumber < 2){
-    addPlayer();
-    $("#player-name").val("");  
-    $(".input-group").css("visibility","hidden");
-    $(".game-info").css("visibility","visible");
-    $("#name").html("Hi: " + game.player.name + " You are Player #: " + game.player.playerNumber + " it's "+ game.turn + " turn");
-    $(".weapons").css("visibility","visible");
-    }
-    else {
-      console.log("Game Full");
-    }
-  
-  
-
-  return false;
-}); //str-btn on click
 
 //read weapon from screen buttons & update firebase
 $(".weapons button").on("click", function() {
@@ -74,25 +54,41 @@ $(".weapons button").on("click", function() {
   //disable buttons once one is chosen
   $(".weapons button").prop("disabled", true);
  
-  //find who won returns the player number ie 1 or 2 or Tie   
-      var winner  = whoWon(weaponsArray);
+  //find whoWon returns the player number ie 1 or 2 or Tie   
+      if(Boolean(weaponsArray[0] && weaponsArray[1])){
+        var winner  = whoWon(weaponsArray);
 
-      if (winner === "Tie") {
-        console.log("The game was a Tie")
-      } else
-      console.log("Player: " + winner + " Won!");
+        if (winner === "Tie") {
+          console.log("The game was a Tie");
+          game.player.ties ++;
+          game.pushKey.update({ties: game.player.ties});
+        } else
+        
+        if(game.player.playerNumber = winner ){
+          
+          console.log("Player: " + winner + " Won!");
+          game.player.wins ++;
+          game.pushKey.update({wins: game.player.wins});
+        }
+        else {
+          console.log("Player: " + winner + " LOST!");
+          game.player.losses ++;
+          game.pushKey.update({losses: game.player.losses});
+
+        }
+      } //if both weapons selected
 
  }); // .weapons on click
 
 
-
 //add  player to firebase  via push method
-function addPlayer(){
+//function addPlayer(){
 
-    playersRefInternal.on("value", function(snapshot) {
+//Detect Connection, push player
+playersRefInternal.on("value", function(snapshot) {
     
     if (snapshot.val().connected) {
-      //push the player on to firebase return key to that player
+      //function to push the player on to firebase return key to that player
       game.pushKey = initPlayer(playersRef);
       
       
@@ -104,22 +100,24 @@ function addPlayer(){
           console.log("The read failed: " + errorObject.code); 
     });
 
+game.pflag = false;
 
 playersRef.on("value", function(snapshot) {
 
-  game.player.playerNumber = false;
   if (snapshot.numChildren() === 1){
     game.player.playerNumber = 1;
+    game.pflag = true;
 
     //update firebase 
     game.pushKey.update({playerNumber: game.player.playerNumber});
 
     }else 
-    if (snapshot.numChildren() === 2 && !game.player.playerNumber){
+    if (snapshot.numChildren() === 2 && !game.pflag){
       game.player.playerNumber = 2;
 
-      //update firebase 
-      game.pushKey.update({playerNumber: game.player.playerNumber});
+
+    //update firebase 
+    game.pushKey.update({playerNumber: game.player.playerNumber});
     }
 
   console.log(game.player.name+" is "+game.player.playerNumber +" Weapon is: " +game.player.weapon);
@@ -133,33 +131,42 @@ function(errorObject) {
   console.log("The read failed: " + errorObject.code); 
   });    
     
-  
-  playersRef.on("value", function(snapshot) {
-  
-    //Add Code here to get what each player played into memory
-    
-    var pkey = "" 
-    var pval = snapshot.val();
-    weaponsArray =[];
-    Object.keys(pval).forEach(function (key) {
-        pkey = pval[key];
-        console.log("Player: " + pkey['playerNumber'] + "   Weapon: " + pkey['weapon']);
-        weaponsArray.push(pkey['weapon']);
-        
-
-    });
-    console.log(weaponsArray);
-
-    
-  },
-  function(errorObject) {
-    console.log("The read failed: " + errorObject.code);
-    });
 
  
   
 
-} //function addPlayer
+//} //function addPlayer
+
+
+
+
+playersRef.on("value", function(snapshot) {
+
+  var pkey = "" 
+  var pval = snapshot.val();
+  weaponsArray =[];
+  Object.keys(pval).forEach(function (key) {
+    pkey = pval[key];
+    game.player.chat = pkey['chat'];
+    game.player.losses = pkey['losses'];
+    game.player.name = pkey['name'];
+    game.player.playerNumber = pkey['playerNumber'];
+    game.player.ties = pkey['ties'];
+    game.player.weapon = pkey['weapon'];
+    game.player.wins = pkey['wins'];
+
+    console.log("Player Name: " + pkey['name'] + " Player Number: " + pkey['playerNumber'] + "   Weapon: " + pkey['weapon']);
+    weaponsArray.push(pkey['weapon']);
+  }); // for each
+console.log(weaponsArray);
+
+
+},
+function(errorObject) {
+console.log("The read failed: " + errorObject.code);
+
+}); // on value
+
 
 
 
@@ -188,18 +195,39 @@ function whoWon(tools){
 
 // Initialize Firebase
 function initPlayer(player){
- 
-  var pushKey = player.push({  
-  name:           game.player.name,
-  playerNumber:   0,
-  wins:           0,
-  losses:         0,
-  ties:           0,
-  weapon:         "",
-  chat:           ""
   
+  //Get username from console and add to firebase
+  $("#start-btn").on("click", function() {
+    game.player.name = $("#player-name").val().trim();
+    console.log(game.player.name);
+    
+    // update player 1 in firebase
+    game.pushKey.update({name: game.player.name});
+    
+    
+    //paint Player name div only after playerNumber is captured
+    $("#player-name").val("");  
+    $(".input-group").css("visibility","hidden");
+    $(".game-info").css("visibility","visible");
+    
+    $("#name").html("Hi: " + game.player.name + "! You are Player #: " + game.player.playerNumber + " It's "+ game.turn + " Turn");
+    $(".weapons").css("visibility","visible");
+  return false;
+  }); //str-btn on click
+
+
+  var pushKey = player.push({  
+        name:           game.player.name,
+        playerNumber:   game.player.playerNumber,
+        wins:           game.player.wins,
+        losses:         game.player.losses,
+        ties:           game.player.ties,
+        weapon:         game.player.weapon,
+        chat:           game.player.chat
   }); //fb push 
-  return pushKey
+  
+ return pushKey
+
 } //function setFireBase
 
   
